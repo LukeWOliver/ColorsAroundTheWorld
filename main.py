5ce47d46c577c20dbd7d79fc0df72d26eaefd88d
Luke Oliver <140974464+LukeWOliver@users.noreply.github.com>
2023-10-15 23:01:45 -0500

    Update main.py
    Updated comments

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 """
 Luke Oliver & Vighnesh Koikal 10/13/23      
-This is a program that helps kids learn colors in different languages (Spanish and French at the moment)
+This is the main script which controls which langauge module is engaged
+using a function and function calls
 """
 
 import spanish
 
+# Function for the main/opening menu - LWO & VK
 def Menu():
     language = ""
     languages = ["Spanish", "French"]
     
-    # Print welcome message
+    # Print welcome message - LWO & VK
     print("Welcome to Colors Around the World!\n")
     print("This program aims to teach you the basics of different languages.")
     print("We currently have " + str(len(languages)) + " available  languages:\n")
     
-    # Print each available language
+    # Print each available language - LWO
     for i in range(len(languages)):
         print(languages[i])
     print()
     
     # While loop containing language selection that prevents
-    # the program from crashing given an unusable input
+    # the program from crashing given an unusable input - LWO & VK
     while language != "spanish" and language != "french" and language != "quit":
         language = str(input("Please enter a language you would like to pracitce (if you would like to exit the program, type \"quit\"): ")).lower()
 
-        # Check which language is chosen and run the function for said language
+        # Check which language is chosen and run the function for said language - VK
         if language == "spanish":
             spanish.Spanish()
-        #elif language == "french":
-            #french.French()
+        elif language == "french":
+            french.French()
         elif language == "quit":
             break
     
-    # Clear console
+    # Create whitespace - LWO
     print()
 
 Menu()
